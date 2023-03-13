ef1628091e54c9937bb0b743ffe983ff37703034
meatjam <851404658@qq.com>
2023-03-13 16:19:04 +0800

    增加chatGPT模块下的web智能搜索功能

--- LittlePaimon/plugins/Chat_GPT/__init__.py
+++ LittlePaimon/plugins/Chat_GPT/__init__.py
@@ -1,5 +1,6 @@
 import requests
 from typing import Union
+from re import findall
 from nonebot import on_command
 from nonebot.exception import FinishedException
 from nonebot.plugin import PluginMetadata
@@ -7,7 +8,7 @@ from nonebot.adapters.onebot.v11 import Message, GroupMessageEvent, PrivateMessa
 from nonebot.params import CommandArg
 from nonebot.rule import to_me
 
-from LittlePaimon.plugins.Chat_GPT.api_request import get_completions, get_chat_completions
+from LittlePaimon.plugins.Chat_GPT.api_request import get_completions, get_chat_completions, get_web_search
 from LittlePaimon.utils.message import CommandCharacter, CommandLang, MessageBuild
 
 
@@ -35,6 +36,14 @@ chat_gpt = on_command('chat', priority=8, block=True, rule=to_me(), state={
     'pm_priority': 3
 })
 
+web_search = on_command('web', priority=8, block=True, rule=to_me(), state={
+    'pm_name': '联网的智能化Web搜索',
+    'pm_description': '联网的智能化Web搜索',
+    'pm_usage': '@Bot web ([期望返回数]默认3条) [搜索内容]',
+    'pm_priority': 3
+})
+
+
 # is_thinking = False
 
 
@@ -56,3 +65,30 @@ async def _(event: Union[GroupMessageEvent, PrivateMessageEvent], msg: Message =
         await chat_gpt.finish(f'出错了，请稍后重试。{e}')
     # finally:
     #     is_thinking = False
+
+
+@web_search.handle()
+async def _(event: Union[GroupMessageEvent, PrivateMessageEvent], msg: Message = CommandArg()):
+    if not event.to_me:
+        return
+    msg_input = msg.extract_plain_text().strip()
+    number_index = msg_input.find(' ')
+    first_input = msg_input[:number_index]
+    if len(findall(r'\D', first_input)) > 0:
+        msg = msg_input
+        max_results = 3
+    else:
+        msg = msg_input[number_index:].strip()
+        max_results = int(first_input)
+    try:
+        result = await get_web_search(msg, max_results)
+        output_strings = []
+        i = 1
+        for content in result:
+            output_strings.append(f'[{i}] "{content["body"]}"\n{content["href"]}\n')
+            i += 1
+        await chat_gpt.finish('\n'.join(output_strings))
+    except FinishedException:
+        pass
+    except Exception as e:
+        await chat_gpt.finish(f'出错了，请稍后重试。{e}')
--- LittlePaimon/plugins/Chat_GPT/api_request.py
+++ LittlePaimon/plugins/Chat_GPT/api_request.py
@@ -5,14 +5,21 @@ from requests_async import Session
 BASE_URL = 'https://api.openai.com/v1'
 
 
-async def get_session():
+async def get_session_chatgpt():
     session = Session()
     session.headers = {'authorization': f'Bearer {config.chatGPT_APIKEY}'}
     return session
 
 
+async def get_session_web():
+    session = Session()
+    session.headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
+    return session
+
+
 async def get_completions(prompt: str, max_tokens: int = 500, temperature: int = 1):
-    session = await get_session()
+    session = await get_session_chatgpt()
     result = await session.post(f'{BASE_URL}/completions', json={
         "model": "text-davinci-003",  # 该模型是GPT-3中最强的模型
         "prompt": prompt,  # 用户说的话
@@ -30,7 +37,7 @@ async def get_completions(prompt: str, max_tokens: int = 500, temperature: int =
 
 
 async def get_chat_completions(content: str, max_tokens: int = None, temperature: int = 1):
-    session = await get_session()
+    session = await get_session_chatgpt()
     result = await session.post(f'{BASE_URL}/chat/completions', json={
         "model": "gpt-3.5-turbo-0301",  # 该模型是GPT-3中最强的模型
         "messages": [{"role": "user", "content": content}],  # 用户说的话
@@ -45,3 +52,14 @@ async def get_chat_completions(content: str, max_tokens: int = None, temperature
     if answer_result['finish_reason'] == 'length':
         text += f'（目前长度最多只支持{max_tokens}个tokens，超出部分已省略。）'
     return text
+
+
+async def get_web_search(q: str, max_results=3, region='cn-zh'):
+    return (await (await get_session_web()).get(
+        f'https://ddg-webapp-aagd.vercel.app/search',
+        params={
+            'q': q,
+            'max_results': max_results,
+            'region': region
+        }
+    )).json()
