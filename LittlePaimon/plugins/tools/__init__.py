565c87200d197337e018be45bf23534fa7242db5
yuzhe.sun <mi_yuzhe.sun@mihoyo.com>
2023-08-08 18:24:52 +0800

    feat(Chat_GPT): 因安全原因，暂时禁用网页截图功能

--- LittlePaimon/plugins/tools/__init__.py
+++ LittlePaimon/plugins/tools/__init__.py
@@ -24,20 +24,20 @@ __plugin_meta__ = PluginMetadata(
     }
 )
 
-screenshot_cmd = on_command('网页截图', priority=10, block=True, rule=Rule(permission_check), state={
-    'pm_name':        '网页截图',
-    'pm_description': '对指定链接页面进行截图，例：【网页截图www.baidu.com】',
-    'pm_usage':       '网页截图<链接>',
-    'pm_priority':    1
-})
-
-
-@screenshot_cmd.handle()
-async def _(event: MessageEvent, msg: Message = CommandArg()):
-    await screenshot_cmd.send('正在尝试截图，请稍等...')
-    url = msg.extract_plain_text().strip()
-    try:
-        img = await screenshot(url)
-        await screenshot_cmd.send(MessageSegment.image(img))
-    except Exception:
-        await screenshot_cmd.send('尝试网页截图失败，无法访问该网页，请稍候再试')
+# screenshot_cmd = on_command('网页截图', priority=10, block=True, rule=Rule(permission_check), state={
+#     'pm_name':        '网页截图',
+#     'pm_description': '对指定链接页面进行截图，例：【网页截图www.baidu.com】',
+#     'pm_usage':       '网页截图<链接>',
+#     'pm_priority':    1
+# })
+#
+#
+# @screenshot_cmd.handle()
+# async def _(event: MessageEvent, msg: Message = CommandArg()):
+#     await screenshot_cmd.send('正在尝试截图，请稍等...')
+#     url = msg.extract_plain_text().strip()
+#     try:
+#         img = await screenshot(url)
+#         await screenshot_cmd.send(MessageSegment.image(img))
+#     except Exception:
+#         await screenshot_cmd.send('尝试网页截图失败，无法访问该网页，请稍候再试')
